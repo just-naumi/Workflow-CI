9c60b99d1297d47e042bb7379aa9d79372f64252
just_naumi <m.naufal.hazmi@gmail.com>
2026-06-30 20:32:49 +0700

    workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,31 +3,28 @@ import mlflow.sklearn
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import dagshub # Tambahkan ini supaya DagsHub-nya terinisialisasi dengan benar
 
-# 1. Setup MLflow Lokal
-# Kriteria Basic: Disimpan secara lokal
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+# Inisialisasi DagsHub agar koneksi lebih stabil di GitHub Actions
+dagshub.init(repo_owner="just-naumi", repo_name="Eksperimen_SML_naumi", mlflow=True)
+
+# Ganti ke URI DagsHub
+mlflow.set_tracking_uri("https://dagshub.com/just-naumi/Eksperimen_SML_naumi.mlflow")
 mlflow.set_experiment("Latihan Credit Scoring")
 
-# 2. AKTIFKAN AUTOLOG
-# INI WAJIB ADA untuk memenuhi syarat Basic
+# AKTIFKAN AUTOLOG
 mlflow.sklearn.autolog()
 
-# 3. Load Data Bersih
-# Pastikan path ini sesuai dengan folder di dalam folder Membangun_model
+# Load Data Bersih
 df = pd.read_csv('namadataset_preprocessing/data_bersih.csv')
 X = df.drop('loan_status', axis=1)
 y = df['loan_status']
 
-# 4. Training Sederhana
+# Training Sederhana
 with mlflow.start_run():
-    # Tanpa hyperparameter tuning, pakai default saja
     model = RandomForestClassifier()
-    
-    # MLflow akan otomatis mencatat parameter dan metrik lewat autolog()
     model.fit(X, y)
     
-    # Print sekilas saja
     predictions = model.predict(X)
     acc = accuracy_score(y, predictions)
     print(f"Model trained with accuracy: {acc}")
